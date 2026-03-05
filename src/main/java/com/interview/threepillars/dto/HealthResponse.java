ea9d281bd7474d4605d4ad38989836031f71d43e
Billy Rojas Vindas <53454194+billyjoseph008@users.noreply.github.com>
2026-03-05 15:42:42 -0800

    chore: empty baseline for review

--- src/main/java/com/interview/threepillars/dto/HealthResponse.java
+++
@@ -1,9 +0,0 @@
-package com.interview.threepillars.dto;
-
-import lombok.Getter;
-
-@Getter
-public class HealthResponse {
-    private final String status;
-    public HealthResponse(String status) { this.status = status; }
-}
--- src/main/java/com/interview/threepillars/repository/RandomUserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.interview.threepillars.repository;
-
-import com.interview.threepillars.model.RandomUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RandomUserRepository extends JpaRepository<RandomUser, Integer> {
-
-}
